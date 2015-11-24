193e169f6d9c1db05706dc548a61a5021a89c9b6
Riccardo Nobile <riccardo.nobile@winfuture.it>
2015-11-24 09:30:41 +0100

    homework_04.js fix November 24, 2015 @ 09:30 Update

--- homework_04.js
+++ homework_04.js
@@ -1,4 +1,51 @@
+function Stack() {
+    this.myarray = [];
+}
 
-void function rpn(string) {
+Stack.prototype.push = function(x) {
+    this.myarray.push(x);
+}
 
+Stack.prototype.pop = function() {
+    return this.myarray.pop();
+}
+
+Stack.prototype.peek = function() {
+    return this.myarray[this.myarray.length - 1];
+}
+
+Stack.prototype.isEmpty = function() {
+    return this.myarray.length == 0;
+}
+
+void function rpn(str) {
+    var stack = new Stack();
+    var strArr = str.split(" ");
+
+    strArr.forEach(function(el) {
+        var intEl = parseInt(el);
+        if(!isNaN(intEl)) {
+            stack.push(intEl);
+        } else {
+            switch(el) {
+                case "+":
+                    var op2 = stack.pop();
+                    stack.push(stack.pop() + op2);
+                    break;
+                case "-":
+                    var op2 = stack.pop();
+                    stack.push(stack.pop() - op2);
+                    break;
+                case "*":
+                    var op2 = stack.pop();
+                    stack.push(stack.pop() * op2);
+                    break;
+                case "/":
+                    var op2 = stack.pop();
+                    stack.push(stack.pop() / op2);
+                    break;
+            }
+        }
+    });
+    return stack.myarray[0];
 }
