c26d7637e071edd7d31a5b3668da6a357d8e435f
Riccardo Nobile <riccardo.nobile@winfuture.it>
2015-11-27 09:07:06 +0100

    Fix homework_06.js November 27, 2015 @ 9:06 Update

--- homework_06.js
+++ homework_06.js
@@ -1,1 +1,10 @@
-
+function sort(myarray) {
+    if (myarray.length == 0 || myarray.length == 1) {
+        return myarray;
+    } else {
+        var n = Math.cell(myarray / 2);
+        var primo = myarray.slice(0, n);
+        var secondo = myarray.slice(n, myarray.length);
+        return sort(primo).concat(sort(secondo));
+    }
+}
